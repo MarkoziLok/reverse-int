2c1b62272d4bb95f0e531b2344d29c8e556fab47
MarkoziLok <markloktionov77@gmail.com>
2023-02-21 16:28:30 +0400

    add: reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+    // n = n + "";
+    // n = +n.split("").reverse().join("");
+    n = Math.abs(n) +""
+    n = +n.split("").reverse().join("");
+    return n
 }
